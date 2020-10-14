973f5993bee68bde97a6396b3ce84f52301a4a66
Dhruv Rawat <dhruv@pankhuri.co>
2020-10-14 13:09:08 +0530

    Added dark mode

--- app/src/main/java/com/example/laundryapp/HomeActivity.java
+++ app/src/main/java/com/example/laundryapp/HomeActivity.java
@@ -1,15 +1,21 @@
 package com.example.laundryapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class HomeActivity extends AppCompatActivity {
+    Switch aSwitch;
     Button btnlogout;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -18,6 +24,38 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        aSwitch=findViewById(R.id.aswitch);
+
+        int defaultNightMode = AppCompatDelegate.getDefaultNightMode();
+        if(defaultNightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            aSwitch.setChecked(true);
+        }
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(buttonView.isPressed() && isChecked)
+                {
+                    //if day mode is enabled, set night mode using AppCompatDelegate class.
+                    Toast.makeText(HomeActivity.this, "Night selected", Toast.LENGTH_SHORT).show();
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    Intent intent = new Intent(HomeActivity.this , HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                else if (buttonView.isPressed() && !isChecked){
+                    //if night mode is enabled, set day mode using AppCompatDelegate class.
+                    Toast.makeText(HomeActivity.this, "Day mode selected", Toast.LENGTH_SHORT).show();
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    Intent intent = new Intent(HomeActivity.this , HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+
+
+                }
+            }
+        });
         btnlogout = findViewById(R.id.logout);
         btnlogout.setOnClickListener(new View.OnClickListener() {
             @Override
